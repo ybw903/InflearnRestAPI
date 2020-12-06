520b2092ddef2a03245b9535f3a90641118ca68e
병우 유 <conner903@naver.com>
2020-12-06 17:59:32 +0900

    project creation

---
+++ src/test/java/study/spring/demostudyrestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.demostudyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
