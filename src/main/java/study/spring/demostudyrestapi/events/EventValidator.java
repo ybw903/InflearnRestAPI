27732cc3bc049449e6a30f29c9817ecee912275b
병우 유 <conner903@naver.com>
2020-12-07 20:45:27 +0900

    validate wrong data

---
+++ src/main/java/study/spring/demostudyrestapi/events/EventValidator.java
@@ -0,0 +1,40 @@
+package study.spring.demostudyrestapi.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDto eventDto, Errors errors){
+        if(eventDto.getBasePrice()>eventDto.getMaxPrice()&& eventDto.getMaxPrice()>0){
+            errors.rejectValue("basePrice","wrongValue","BasePrice is wrong.");
+            errors.rejectValue("maxPrice","wrongValue","MaxPrice is wrong.");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if(endEventDateTime.isBefore(eventDto.getBeginEventDateTime())||
+                endEventDateTime.isBefore(eventDto.getBeginEventDateTime())||
+                endEventDateTime.isBefore(eventDto.getBeginEventDateTime())){
+            errors.rejectValue("endEventDateTime","wrongValue","endEventDateTime is wrong.");
+        }
+
+        //TODO BeginEventDateTime
+        LocalDateTime beginEventDateTime = eventDto.getBeginEventDateTime();
+        if(beginEventDateTime.isAfter(eventDto.getEndEventDateTime())||
+                beginEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())||
+                beginEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime())){
+            errors.rejectValue("beginEventDateTime","wrongValue","beginEventDateTime is wrong.");
+        }
+
+        //TODO CloseEnrollmentDateTime
+        LocalDateTime closeEnrollmentDateTime = eventDto.getCloseEnrollmentDateTime();
+        if(closeEnrollmentDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())||
+                closeEnrollmentDateTime.isAfter(eventDto.getBeginEventDateTime())||
+                closeEnrollmentDateTime.isAfter(eventDto.getEndEventDateTime())){
+            errors.rejectValue("closeEnrollmentDateTime","wrongValue","closeEnrollmentDateTime is wrong.");
+        }
+    }
+}
--- src/test/java/study/spring/demostudyrestapi/events/EventControllerTests.java
+++ src/test/java/study/spring/demostudyrestapi/events/EventControllerTests.java
@@ -118,4 +118,26 @@ public class EventControllerTests {
                     )
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,12,10,14,7))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,12,9,14,7))
+                .beginEventDateTime(LocalDateTime.of(2020,12,8,14,7))
+                .endEventDateTime(LocalDateTime.of(2020,12,7,14,7))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto))
+        )
+                .andExpect(status().isBadRequest());
+    }
 }
